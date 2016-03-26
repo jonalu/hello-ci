55ea186c8742f5e662c9bc4d602b68badbe7e48e
jlu <jlundela@gmail.com>
2016-03-26 14:17:40 +0100

    Socket IO implementation

--- server.js
+++ server.js
@@ -5,8 +5,36 @@ var express = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io')(server);
 
+var sockets = []
+
+var request = require('superagent');
+require('superagent-as-promised')(request);
+
+function poll () {
+  request.get('http://rest.tv2.no:80/sports-dw-rest/sport/event?tournamentId=1%2C230%2C232&seasonId=337&content=summary&max=50')
+  .then(res => res.body)
+  .then(events => sockets.forEach( socket => socket.emit('events', events)))
+  .then(() => {
+    setTimeout(() => poll(), 5000)
+  })
+  .catch(err => {
+    setTimeout(() => poll(), 5000)
+  })
+}
+
+poll()
+
 io.on('connection', (socket) => {
-  socket.on('pingo', (msg) => io.emit('ping', msg))
+
+  sockets.push(socket)
+
+  socket.on('disconnect', () => {
+    sockets.splice(sockets.indexOf(socket), 1)
+  })
+
+  request.get('http://rest.tv2.no/sports-dw-rest/sport/schedule?fromDate=2016-03-26T00%3A00%3A00%2B01%3A00&toDate=2016-03-26T23%3A59%3A00%2B01%3A00')
+  .then(res => res.body)
+  .then(schedule => socket.emit('schedule', schedule))
 })
 
 app.get('/', (req, res) =>  res.render('index'))
