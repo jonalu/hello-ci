a1a4916d91a873104a46a46003b2ed80afe4524d
jlu <jlundela@gmail.com>
2016-03-26 13:24:12 +0100

    Run es6 on server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+'use strict'
 var express = require('express'),
     app = express(),
-    middleware = require('./middleware')
+    middleware = require('./middleware'),
+    server = require('http').createServer(app),
+    io = require('socket.io')(server);
 
-app.get('/ping', middleware.ping)
-app.get('/', middleware.helloWorld)
+io.on('connection', (socket) => {
+  socket.on('pingo', (msg) => io.emit('ping', msg))
+})
 
+app.get('/', (req, res) =>  res.render('index'))
+
+app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000)
 
-app.listen(app.get('port'), function() {
-  console.log('Application listening on port', app.get('port'))
-})
+server.listen(app.get('port'), () => console.log('Application listening on port', app.get('port')))
