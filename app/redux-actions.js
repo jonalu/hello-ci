959be32e5de867e71bc5034f0683a97f8de2bcc4
Mads André Lundeland <mads.lundeland@vimond.com>
2016-03-28 20:34:46 +0200

    Toggle tournaments

--- app/redux-actions.js
+++ app/redux-actions.js
@@ -1,18 +1,18 @@
 'use strict'
-import moment from 'moment'
-import request from 'superagent'
-import superAgentAsPromised from 'superagent-as-promised'
+import moment from "moment";
+import request from "superagent";
+import superAgentAsPromised from "superagent-as-promised";
 
 superAgentAsPromised(request)
 
-export function updateTournaments (tournaments) {
- return {
-   type: 'RECEIVE_TOURNAMENTS',
-   tournaments
- }
+export function updateTournaments(tournaments) {
+  return {
+    type: 'RECEIVE_TOURNAMENTS',
+    tournaments
+  }
 }
 
-export function fetchTournaments (day = moment(new Date()).format('YYYY-MM-DD')) {
+export function fetchTournaments(day = moment(new Date()).format('YYYY-MM-DD')) {
   return (dispatch, getState) => {
     return request.get(`http://rest.tv2.no/sports-dw-rest/sport/football/schedule?fromDate=${day}T00%3A00%3A00%2B01%3A00&toDate=${day}T23%3A59%3A00%2B01%3A00`)
       .then(res => res.body)
@@ -25,34 +25,33 @@ export function fetchTournaments (day = moment(new Date()).format('YYYY-MM-DD'))
 }
 
 export const groupByTournament = schedule => {
-  const tournamentIds = schedule.matches
+  return schedule.matches
     .map(m => m.tournament.id)
     .reduce((tournaments, id) => {
       if (tournaments.indexOf(id) === -1) {
         tournaments.push(id);
       }
       return tournaments;
-    }, []);
-
-  return tournamentIds
-  .map(id => {
-    const matches = schedule.matches
-    .filter(m => m.tournament.id === id)
-    return {
-      id: id,
-      name: matches[0].tournament.name,
-      matches: matches.map(m => {
-        return {
-          id: m.id,
-          teamA: m.teamA,
-          teamB: m.teamB,
-          matchStatus: m.matchStatus,
-          startTime: moment(m.startTime).format('HH:mm'),
-          playTime: timePlayed(m)
-        }
-      })
-    }
-  })
+    }, [])
+    .sort((a, b) => parseInt(a) - parseInt(b))
+    .map(id => {
+      const matches = schedule.matches
+        .filter(m => m.tournament.id === id)
+      return {
+        id: id,
+        name: matches[0].tournament.name,
+        matches: matches.map(m => {
+          return {
+            id: m.id,
+            teamA: m.teamA,
+            teamB: m.teamB,
+            matchStatus: m.matchStatus,
+            startTime: moment(m.startTime).format('HH:mm'),
+            playTime: timePlayed(m)
+          }
+        })
+      }
+    })
 }
 
 export const timePlayed = match => {
@@ -68,4 +67,4 @@ export const timePlayed = match => {
   }
 }
 
-export default { updateTournaments }
+export default {updateTournaments}
